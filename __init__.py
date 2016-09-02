ce56e0a2b82511f3d08e245f5873877fe87e267c
daryl <dladd10@gmail.com>
2016-09-01 21:39:10 -0400

    update 1

--- __init__.py
+++ __init__.py
@@ -1,31 +1,37 @@
-import datetime
 from os.path import dirname
 
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill
 from mycroft.util.log import getLogger
 
+import datetime
 # import tzlocal
 # from astral import Astral
 # from pytz import timezone
 
-__author__ = `dladd10`
+#__author__ = `dladd10`
+
+LOGGER = getLogger(__name__)
 
 class GoodMorningSkill(MycroftSkill):
+
     def __init__(self):
         super(GoodMorningSkill, self).__init__(name="GoodMorningSkill")
 
     def initialize(self):
         self.load_data_files(dirname(__file__))
 
-	#.voc files
-        GoodMorningIntent = IntentBuilder("GoodMorningIntent").require("GoodMorning").build()
-	self.register_intent(good_morning_intent, self.handle_good_morning_reply_intent)
+		#.voc files
+        goodmorningintent = IntentBuilder("GoodMorningIntent").\
+            require("goodmorningkeyword").build()
+
+        self.register_intent(goodmorningintent, self.handle_goodmorningintent)
 
     #What Mycroft is to do
-    def handle_good_morning_reply_intent(self, message):
-	#Say Good Morning
-	self.speak_dialog("GoodMorningKeywords")
+    def handle_goodmorningintent(self, message):
+        # TODO Say Good Morning
+        self.speak_dialog("GoodMorningReply")
+
 	#Tell you the date
 	##todaydate = datetime.datetime.now().strftime("%B %d, %Y")
 	##today = datetime.datetime.strptime(todaydate, '%B %d, %Y').strftime('%A')
@@ -47,8 +53,8 @@ class GoodMorningSkill(MycroftSkill):
 	##self.speak("Today is " + today)
 
 
-    def stop(self):
-        pass
+	def stop(self):
+        	pass
 
-def create_skill():
-    return GoodMorningSkill()
+	def create_skill():
+		return GoodMorningSkill()
